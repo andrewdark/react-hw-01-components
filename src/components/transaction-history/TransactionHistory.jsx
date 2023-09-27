23736d925fb5829738f50265f4916499b35c2a12
andrewdark <samsonov.a@ukr.net>
2023-09-27 11:49:52 +0300

    add tx-history component jsx

---
+++ src/components/transaction-history/TransactionHistory.jsx
@@ -0,0 +1,26 @@
+import css from './TransactionHistory.module.css';
+export const TransactionHistory = ({ th }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {th.map(elem => {
+          return (
+            <tr id={elem.id}>
+              <td>{elem.type}</td>
+              <td>{elem.amount}</td>
+              <td>{elem.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
